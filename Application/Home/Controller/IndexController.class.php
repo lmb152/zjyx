ab71b61ccf23173963bef04df5a04205c2569ccc
martin <martin@eprotime.com>
2017-11-03 13:54:11 +0800

    静态页面

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -11,88 +11,27 @@ class IndexController extends Controller {
         }  
     }
     public function index(){
-        // $article=M('article');
-        // $artkind=M('artkind');
-        // // 校园动态
-        // $condition['name']='校园动态';
-        // $artkind_temp=$artkind->field('id')->where($condition)->find();
-        // unset($condition);
-        // $condition['artkind']=$artkind_temp['id'];
-        // $condition['isindex']='1';
-        // $condition['ispassed']='1';
-        // $school_news=$article->where($condition)->limit(0,8)->order('released desc')->select();
-        // $this->assign('school_news_kind',$artkind_temp['id']);
-        // $this->assign('school_news',$school_news);
-        // $condition['imgurl']=array('NEQ','');
-        // $school_news_img=$article->where($condition)->limit(0,5)->order('released desc')->select();
-        // $this->assign('school_news_img',$school_news_img);
-        // // 通知公告
-        // $condition['name']='通知公告';
-        // $artkind_temp=$artkind->field('id')->where($condition)->find();
-        // unset($condition);
-        // $condition['artkind']=$artkind_temp['id'];
-        // $condition['isindex']='1';
-        // $condition['ispassed']='1';
-        // $announcement=$article->where($condition)->limit(0,8)->order('released desc')->select();
-        // $this->assign('announcement_kind',$artkind_temp['id']);
-        // $this->assign('announcement',$announcement);
-        // // 党建风采
-        // $condition['name']='党建风采';
-        // $artkind_temp=$artkind->field('id')->where($condition)->find();
-        // unset($condition);
-        // $condition['artkind']=$artkind_temp['id'];
-        // $condition['isindex']='1';
-        // $condition['ispassed']='1';
-        // $party_building=$article->where($condition)->limit(0,6)->order('released desc')->select();
-        // $this->assign('party_building_kind',$artkind_temp['id']);
-        // $this->assign('party_building',$party_building);
-        // // 学习园地
-        // $condition['name']='学习园地';
-        // $artkind_parent=$artkind->field('id')->where($condition)->find();
-        // // unset($condition);
-        // // $condition['parent']=$artkind_parent['id'];
-        // $condition['artkind']=$artkind_parent['id'];
-        // $condition['isindex']='1';
-        // // $artkind_temp=$artkind->field('id')->where($condition)->select();
-        // // unset($condition);
-        // // foreach ($artkind_temp as $key => $value) {
-        // //     if(($key+1)==count($artkind_temp)){
-        // //         $temp .=$value['id'];
-        // //     }else{
-        // //        $temp .=$value['id'].','; 
-        // //     } 
-        // // }
-        // // $condition['artkind']=array('in',$temp);
-        // // $condition['isindex']='1';
-        // $condition['ispassed']='1';
-        // $study_center=$article->where($condition)->limit(0,6)->order('released desc')->select();
-        // $this->assign('study_center_kind',$artkind_temp['id']);
-        // $this->assign('study_center',$study_center);
-        // // 佳作欣赏
-        // $condition['name']='佳作欣赏';
-        // $artkind_temp=$artkind->field('id')->where($condition)->find();
-        // unset($condition);
-        // $condition['artkind']=$artkind_temp['id'];
-        // $condition['isindex']='1';
-        // $condition['ispassed']='1';
-        // $art_works=$article->where($condition)->order('released desc')->select();
-        // $this->assign('art_works',$artkind_temp['id']);
-        // $this->assign('art_works',$art_works);
-        // // 其他活动
-        // $condition['name']='其他活动';
-        // $artkind_temp=$artkind->field('id')->where($condition)->find();
-        // unset($condition);
-        // $condition['artkind']=$artkind_temp['id'];
-        // $condition['isindex']='1';
-        // $condition['ispassed']='1';
-        // $banner=$article->where($condition)->order('released desc')->find();
-        // $this->assign('banner',$artkind_temp['id']);
-        // $this->assign('banner',$banner);
-
     	$this->display();
     } 
-    public function logout() {
-        unset($_SESSION['uname']);
-        $this->redirect('Webadmin/index/login');
+    // 手机绑定
+    public function bind_phone(){
+        $this->redirect('index/basic_info');
+    }
+    public function basic_info(){
+        $this->display();
+    } 
+    // 基本信息保存
+    public function basic_info_save(){
+        $this->redirect('index/detail_info');
+    }
+    public function detail_info(){
+        $this->display();
+    } 
+    // 详细信息保存
+    public function detail_info_save(){
+        $this->redirect('index/info_finish');
+    }
+    public function info_finish(){
+        $this->display();
     }
 }
