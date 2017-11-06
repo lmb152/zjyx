470149929d57081ad2b59131e53fa6432446cd39
martin <martin@eprotime.com>
2017-11-06 23:07:40 +0800

    微信授权登陆返回openid

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,15 +2,38 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function _initialize(){
+    public function index(){
         // 获取用户openid
         $info=$_SESSION['memberinfo'];
         if(!isset($info->openid)){
             // 授权登录获取openid
-            // $this->redirect('/Wechat');
-        }  
-    }
-    public function index(){
+            $this->redirect('/Wechat');
+        }else{
+            $user_profile=D('user_profile');
+            $where=array(
+                    'openid'=>$info->openid,
+                );
+            $user_data_temp = $user_profile->where($where)->find();
+            if(!$user_data_temp){
+                if($info->subscribe){
+                    $user_data=array(
+                        'openid'=>$info->openid,
+                        'imgurl'=>$info->headimgurl,
+                    );
+                }else{
+                    $user_data=array(
+                        'openid'=>$info->openid,
+                    );
+                }
+                $user_profile->data($user_data)->add();
+            }else{
+                $user_data=array(
+                    'imgurl'=>$info->headimgurl,
+                );
+                $user_profile->where($where)->save($user_data);
+            }
+        }
+        $this->assign('openid',$info->openid);
     	$this->display();
     } 
     // 手机绑定
