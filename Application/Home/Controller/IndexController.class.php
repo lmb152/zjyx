f0904a07c427303e17a702405d6e569a90f6eecf
martin <martin@eprotime.com>
2017-11-08 14:22:22 +0800

    详细信息填写

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,13 @@ namespace Home\Controller;
 use Think\Controller;
 
 class IndexController extends Controller {
+	public function _initialize(){
+		$info = $_SESSION['memberinfo'];
+		if (!isset($info->openid)) {
+			// 授权登录获取openid
+			$this->redirect('/Wechat');
+		}
+	}
 	public function index() {
 		// 获取用户openid
 		$info = $_SESSION['memberinfo'];
@@ -137,11 +144,12 @@ class IndexController extends Controller {
 			);
 		$user_army_data=$army->where($condition)->select();
 		$user_education_data=$education->where($condition)->select();
-		$user_data['army_data']=$user_army_data;
-		$user_data['education_data']=$user_education_data;
-		$user_data['army_num']=count($user_army_data);
-		$user_data['education_num']=count($user_education_data);
+
 		$this->assign('user_data', $user_data);
+		$this->assign('army_data', $user_army_data);
+		$this->assign('education_data', $user_education_data);
+		$this->assign('army_num', count($user_army_data));
+		$this->assign('education_num', count($user_education_data));
 		$this->display();
 	}
 	// 详细信息保存
@@ -159,19 +167,162 @@ class IndexController extends Controller {
 		if(!$data['self_evaluation']){
 			$this->error('自我评价不能为空');
 		}
+
+		$user_profile=D('user_profile');
+		$user_army=D('user_army');
+		$user_education=D('user_education');
+
 		$info = $_SESSION['memberinfo'];
 		$profile_data=array(
-				'target_position'=>$data['target_position'],
-				'expected_salary'=>$data['expected_salary'],
+				'target_position'=>trim($data['target_position']),
+				'expected_salary'=>trim($data['expected_salary']),
 				'duty_time'=>strtotime($data['duty_time']),
-				'honor'=>$data['honor'],
-				'self_evaluation'=>$data['self_evaluation']
+				'honor'=>trim($data['honor']),
+				'self_evaluation'=>trim($data['self_evaluation'])
 			);
-		$user_profile=D('user_profile');
+		
 		$condition=array(
 				'openid'=>$info->openid,
 			);
-		$user_profile->where($condition)->save($profile_data);
+		$back=$user_profile->where($condition)->save($profile_data);
+		$user_data=$user_profile->field('id')->where($condition)->find();
+		$uid=$user_data['id'];
+		// 保存原来的工作经历
+		$army_data=array();
+		foreach ($data['service_start'] as $key => $value) {
+			if($value){
+				$army_data[$key]['service_start']=$value;
+			}
+		}
+		foreach ($data['service_end'] as $key => $value) {
+			if($value){
+				$army_data[$key]['service_end']=$value;
+			}
+		}
+		foreach ($data['department'] as $key => $value) {
+			if($value){
+				$army_data[$key]['department']=$value;
+			}
+		}
+		foreach ($data['position'] as $key => $value) {
+			if($value){
+				$army_data[$key]['position']=$value;
+			}
+		}
+		foreach ($army_data as $key => $value) {
+			$update_data=array(
+					'start_time'=>strtotime($value['service_start']),
+					'end_time'=>strtotime($value['service_end']),
+					'department'=>trim($value['department']),
+					'position'=>trim($value['position'])
+				);
+			$condition=array(
+					'army_id'=>$key,
+				);
+			$user_army->where($condition)->save($update_data);
+		}
+
+		// 新建添加的工作经历
+		$new_army_data=array();
+		foreach ($data['new_service_start'] as $key => $value) {
+			if($value){
+				$new_army_data[$key]['service_start']=$value;
+			}
+		}
+		foreach ($data['new_service_end'] as $key => $value) {
+			if($value){
+				$new_army_data[$key]['service_end']=$value;
+			}
+		}
+		foreach ($data['new_department'] as $key => $value) {
+			if($value){
+				$new_army_data[$key]['department']=$value;
+			}
+		}
+		foreach ($data['new_position'] as $key => $value) {
+			if($value){
+				$new_army_data[$key]['position']=$value;
+			}
+		}
+		
+		foreach ($new_army_data as $key => $value) {
+			$update_data=array(
+					'start_time'=>strtotime($value['service_start']),
+					'end_time'=>strtotime($value['service_end']),
+					'department'=>trim($value['department']),
+					'position'=>trim($value['position']),
+					'u_id'=>$uid
+				);
+			$user_army->add($update_data);
+		}
+		// 保存原来的教育经历
+		$edu_data=array();
+		foreach ($data['edu_service_start'] as $key => $value) {
+			if($value){
+				$edu_data[$key]['service_start']=$value;
+			}
+		}
+		foreach ($data['edu_service_end'] as $key => $value) {
+			if($value){
+				$edu_data[$key]['service_end']=$value;
+			}
+		}
+		foreach ($data['school'] as $key => $value) {
+			if($value){
+				$edu_data[$key]['department']=$value;
+			}
+		}
+		foreach ($data['degree'] as $key => $value) {
+			if($value){
+				$edu_data[$key]['position']=$value;
+			}
+		}
+		foreach ($edu_data as $key => $value) {
+			$update_data=array(
+					'start_time'=>strtotime($value['service_start']),
+					'end_time'=>strtotime($value['service_end']),
+					'school'=>trim($value['school']),
+					'degree'=>trim($value['degree'])
+				);
+			$condition=array(
+					'edu_id'=>$key,
+				);
+			$user_education->where($condition)->save($update_data);
+		}
+
+		// 新建添加的教育经历
+		$new_eud_data=array();
+		foreach ($data['new_edu_service_start'] as $key => $value) {
+			if($value){
+				$new_edu_data[$key]['service_start']=$value;
+			}
+		}
+		foreach ($data['new_edu_service_end'] as $key => $value) {
+			if($value){
+				$new_edu_data[$key]['service_end']=$value;
+			}
+		}
+		foreach ($data['new_school'] as $key => $value) {
+			if($value){
+				$new_edu_data[$key]['school']=$value;
+			}
+		}
+		foreach ($data['new_degree'] as $key => $value) {
+			if($value){
+				$new_edu_data[$key]['degree']=$value;
+			}
+		}
+		
+		foreach ($new_edu_data as $key => $value) {
+			$update_data=array(
+					'start_time'=>strtotime($value['service_start']),
+					'end_time'=>strtotime($value['service_end']),
+					'school'=>trim($value['school']),
+					'degree'=>trim($value['degree']),
+					'u_id'=>$uid
+				);
+			$user_education->add($update_data);
+		}
 		$this->redirect('index/info_finish');
 	}
 	public function info_finish() {
